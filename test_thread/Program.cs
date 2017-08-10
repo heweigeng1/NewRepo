04baba280c2e51ce080152000fe0517b8eac1974
heweigeng1 <6318275@qq.com>
2017-08-10 18:24:55 +0800

    多线程

---
+++ test_thread/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace test_thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new Test_ThreadStart();
+            test.ParameTh();
+            //test.Demo1();
+            //test.ThInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test_thread/ThreadClass.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace test_thread
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class ThreadClass
+    {
+        public void A()
+        {
+            Console.Write("A");
+        }
+        public void B()
+        {
+            Console.Write("B");
+        }
+        public void ParameC(User user)
+        {
+            Console.WriteLine(user.Name+$":{user.Age++}");
+        }
+        public void ThreadStartTest1(Action action, int thCount)
+        {
+            for (int i = 0; i < thCount; i++)
+            {
+                Thread th = new Thread(new ThreadStart(action));
+                th.Start();
+            }
+        }
+        public void Test2(Action<User> action,User user, int thCount)
+        {
+            for (int i = 0; i < thCount; i++)
+            {
+                var th=new Thread(new ParameterizedThreadStart(obj => action(user)));
+                th.Start(user);
+            }
+        }
+    }
+}
