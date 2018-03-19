ff88ee9ebd60edf766c35bc184f772f24eb86606
heweigeng1 <6318275@qq.com>
2018-03-19 22:11:25 +0800

    update

--- test_Assembly/Program.cs
+++ test_Assembly/Program.cs
@@ -10,6 +10,27 @@ namespace test_Assembly
     {
         static void Main(string[] args)
         {
+            var data = AssemblyUtils.ObjToSortedDictionary(new WxUnifiedOrderModel
+            {
+                appid = "asdfsf",
+                attach = "测试",
+                body = "boooooody",
+                mch_id = "1",
+                nonce_str = "2",
+                notify_url = "3",
+                out_trade_no = "4",
+                sign = "5",
+                spbill_create_ip = "6",
+                total_fee = 0,
+                trade_type = "7",
+            });
+            data.Remove("sign");
+            string str = data.SortedDictionaryToString();
+            string xml = data.SortedDictionaryToXml();
+            Console.WriteLine(str);
+            string signxml = xml.Replace("</xml>", $@"<sign>{123123}</sign></xml>");
+            Console.WriteLine(signxml);
+            Console.ReadKey();
         }
     }
 }
