3175279114cb5f016d84e185a045850aa050d2f3
heweigeng1 <6318275@qq.com>
2018-05-11 18:21:32 +0800

    重载运算符

---
+++ operatorDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace operatorDemo
+{
+    class Program
+    {
+        public class Tbb
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            static void Main(string[] args)
+            {
+                int? num1, num2;
+                num2 = null;
+                List<int?> list = new List<int?>();
+                var obj = new List<Tbb> { new Tbb { id = 0, name = "aaa" } };
+                var tbb = obj.FirstOrDefault(c => c.id > 0);
+                num1 = tbb == null ? default(int?) : tbb.id;
+                Console.WriteLine(num1);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ operatorDemo/Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace operatorDemo
+{
+    public class Ta
+    {
+        public BigInteger Age { get; set; }
+
+        public Ta()
+        { }
+
+        public Ta(BigInteger age)
+        {
+            this.Age = age;
+        }
+
+        public static Ta operator ^(BigInteger
+            c2, Ta stu)
+        {
+            return new Ta(stu.Age ^ c2);
+        }
+        
+    }
+}
