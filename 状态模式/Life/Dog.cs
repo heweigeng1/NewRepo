6cbfdcd548cf8f92044fdc579cc2b38f385a0cb3
heweigeng1 <6318275@qq.com>
2019-05-14 19:24:15 +0800

    状态机

---
+++ 状态模式/Life/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 状态模式.Life
+{
+    public class Dog
+    {
+        public enum EnumLifeState
+        {
+            出生,
+            成年,
+            老年,
+            死亡
+        }
+    }
+}
---
+++ 里式替换原则/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace 里式替换原则
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new B(new A()).Test();
+            Console.ReadKey();
+        }
+    }
+    public class F
+    {
+        public virtual  void Console()
+        {
+            System.Console.WriteLine("F");
+        }
+    }
+    public class A : F
+    {
+        public override void Console()
+        {
+            System.Console.WriteLine("A");
+        }
+    }
+    public class B : F
+    {
+        F _f;
+        public B(F f)
+        {
+            _f = f;
+        }
+        public void Test()
+        {
+            _f.Console();
+        }
+    }
+}
